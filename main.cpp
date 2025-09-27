a3c58639dee24f152e38a028dcdaa86b6992b51d
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-09-27 20:27:31 +0300

    Welcome, dots!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
+#include "tgaimage.h"
+
+// white green red blue yellow
+constexpr TGAColor white = {255, 255, 255, 255}; // bgra,
+constexpr TGAColor green = {0, 255, 0, 255};
+constexpr TGAColor red = {0, 0, 255, 255};
+constexpr TGAColor blue = {255, 0, 0, 255};
+constexpr TGAColor yellow = {255, 255, 0, 255};
 
 int main() {
-  std::cout << "hello" << std::endl;
+  // width and height of canvas
+  constexpr int height = 64, width = 64;
+
+  // three points as x, y coords
+  constexpr int ax = 10, ay = 10;
+  constexpr int bx = 20, by = 20;
+  constexpr int cx = 30, cy = 30;
+
+  // add three points to frame buffer
+  TGAImage image(width, height, TGAImage::RGB);
+  image.set(ax, ay, white);
+  image.set(bx, by, blue);
+  image.set(cx, cy, green);
+
+  // write frame buffer onto the output file
+  image.write_tga_file("framebuffer.tga", false, false);
 
   return 0;
 }
